0290b6f157c1ac59f82ad582285279ffe5c85749
omararra <omarsamirelmokadem@gmail.com>
2024-03-09 10:16:49 +0300

    fixed integration on ios and added more android support

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import { AppRegistry, Platform } from 'react-native';
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
 import App from './App';
+import {name as appName} from './app.json';
 
-AppRegistry.registerComponent('X', () => App);
-
-if (Platform.OS === 'web') {
-    const rootTag = document.getElementById('root') || document.getElementById('X');
-    AppRegistry.runApplication('X', { rootTag });
-}
+AppRegistry.registerComponent(appName, () => App);
